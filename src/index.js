ada26536324890f78c36635c7545c1e5553db7c0
samuel <samuel.poudroux@hotmail.fr>
2020-03-01 14:22:59 +0100

    firstCOmmit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from "react";
+import NavBar from "../NavBar/NavBar";
+import 'antd/dist/antd.css';
+
+
+export default  Toto = () => <h1>TOTOTOTOTTO</h1> 
+// export const Tata = () => <h1>TATATATA</h1> 
+// export const Tutu = () => <h1>TUTUTU</h1> 
+
+
+
