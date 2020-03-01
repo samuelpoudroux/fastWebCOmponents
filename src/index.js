65f55f380d9501bdf34936f235f3acdd4bf0812f
samuel <samuel.poudroux@hotmail.fr>
2020-03-01 14:43:41 +0100

    """

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 
 import React, { Component } from "react";
-import NavBar from "../NavBar/NavBar";
-import 'antd/dist/antd.css';
 
-
-export default  Toto = () => <h1>TOTOTOTOTTO</h1> 
-// export const Tata = () => <h1>TATATATA</h1> 
-// export const Tutu = () => <h1>TUTUTU</h1> 
+export default  {
+    Toto : () => <h1>TOTOTOTOTTO</h1> ,
+    Tata : () => <h1>TATATATA</h1> ,
+    Tutu : () => <h1>TUTUTU</h1> ,
+}
 
 
 
